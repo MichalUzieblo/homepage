e1a5da6359886263dbed61274abd1a7ec7c7600f
MichalUzieblo <michal_uzieblo@op.pl>
2019-07-01 19:03:16 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('siema - to jest strona Michała Uziębło, pozdrawiam 😄');
+const name = 'Michał';
+const age = 33;
+let michal = null;
+console.log(name);
+console.log(age); 
+console.log(michal);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
